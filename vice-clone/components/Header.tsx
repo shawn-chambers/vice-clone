e9b8c080fd895ede8b4da23f3ec0042966a4b34e
shawn-chambers <chambers.shawn.89@gmail.com>
2022-01-21 15:07:57 -0800

    header links and styling

--- vice-clone/components/Header.tsx
+++ vice-clone/components/Header.tsx
@@ -1,4 +1,17 @@
 import styles from '../styles/header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faInstagram, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const links: string[] = [
+  'Video',
+  'TV',
+  'News',
+  'Tech',
+  'Rec Room'
+];
 
 const Header = () => {
   return (
@@ -12,8 +25,34 @@ const Header = () => {
           + English
         </div>
       </div>
-      <div className="main">
-
+      <div className={styles.main}>
+        <div>
+          <FontAwesomeIcon icon={faBars} size='lg' />
+          <Image
+            src="/vice-logo-white.png"
+            alt="Vice"
+            width={80}
+            height={35}
+            layout='fixed' />
+          {links.map((link, i) => {
+            return (
+              <Link href="/" key={`${link}-${i}`}>
+                <a>{link}</a>
+              </Link>
+            )
+          })}
+        </div>
+        <div className={styles.icons}>
+          <a>
+            <FontAwesomeIcon icon={faFacebookSquare} />
+          </a>
+          <a>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a>
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
       </div>
     </div>
   )
