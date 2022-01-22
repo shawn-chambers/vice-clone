a575548f1543b16fbbcee825726a4f5d8b484294
shawn-chambers <chambers.shawn.89@gmail.com>
2022-01-22 14:04:25 -0800

    finished promoList styling and layout

--- vice-clone/components/Promo.tsx
+++ vice-clone/components/Promo.tsx
@@ -1,11 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+/* needed for unsplace src */
+import Link from 'next/link';
 import styles from '../styles/promo.module.scss';
 
 const Promo = () => {
   return (
     <li className={styles.container}>
-      <div className={styles.promo}>
-        Promo Comp
-      </div>
+      <Link href='/'>
+        <a>
+          <div className={styles.promo}>
+            <div className={styles.image}>
+              <img
+                src="https://source.unsplash.com/random/120x120"
+                alt="random pic"
+              />
+            </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                Buy and/or do something
+              </div>
+              <div className={styles.subtitle}>
+                Because we said to
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
     </li>
   )
 }
