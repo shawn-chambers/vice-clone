8ce9e295bb1ada83067549865517eb22c07adbb5
shawn-chambers <chambers.shawn.89@gmail.com>
2022-01-24 10:57:50 -0800

    updated link lists

--- vice-clone/utils/data.tsx
+++ vice-clone/utils/data.tsx
@@ -1,3 +1,10 @@
+export const mainArticle = {
+  category: 'Technology',
+  teaser: "This is the Thing That We Will Talk About in This Article",
+  quote: "This is a quote from the article to my left. I am sure you will enjoy it if you take the time to read it. Or don't, that's ok, too.",
+  url: '/'
+}
+
 export const links: string[] = [
   'Video',
   'TV',
--- vice-clone/utils/interfaces.tsx
+++ vice-clone/utils/interfaces.tsx
@@ -2,3 +2,14 @@ export interface MenuProps {
   menu: boolean
   displayMenu: (value: boolean | ((prevState: boolean) => boolean)) => void;
 }
+
+export interface MainArticle {
+    category: string
+    teaser: string
+    quote: string
+    url: string
+}
+
+export interface MainArticleProps {
+  mainArticle: MainArticle
+}
